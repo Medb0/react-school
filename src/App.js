81ddedb5d08686edac799f202f24830922aaf841
Medb0 <bxcv1230@gmail.com>
2020-05-06 12:13:53 +0900

    react first

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyClock from './Myclock';
+import UserList from './UserList';
+
+
+class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <UserList />
+          <MyClock />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
